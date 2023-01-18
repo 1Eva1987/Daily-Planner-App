f8a19e80e57f7c63e6e384420783f2d413fa74ed
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-01-18 10:52:05 +0000

    index.js code written to show current day at the top of the calender

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("hello");
+var currentDateEl = $("#currentDay");
+
+// Display the current day at the top of the calender
+currentDateEl.text(moment().format("dddd") + ", " + moment().format("MMMM Do"));
